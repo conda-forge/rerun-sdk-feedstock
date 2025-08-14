6d67d3388e2c1f2cbb73082f18fb857d8f9e81b6
ntjohnson1 <24689722+ntjohnson1@users.noreply.github.com>
2025-08-14 12:20:04 -0400

    More flag mangling for mac x86

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,6 +31,8 @@ case "$target_platform" in
     "osx-64")
         export AR="${CONDA_PREFIX}/bin/llvm-ar"
         export CFLAGS_wasm32_unknown_unknown="-isystem $LIBCLANG_INCLUDE -resource-dir $CLANG_RESOURCE_DIR"
+        # Hmm it should use the target specific flags but it doesn't
+        export TARGET_CFLAGS="-isystem $LIBCLANG_INCLUDE -resource-dir $CLANG_RESOURCE_DIR"
         export CC_wasm32_unknown_unknown="${CONDA_PREFIX}/bin/clang"
         export RUST_TARGET="x86_64-apple-darwin"
         ;;
